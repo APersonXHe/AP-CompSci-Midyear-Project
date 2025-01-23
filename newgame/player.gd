6a3ebf5135e9d3e43641635863117820676cd664
leol07 <litianle772008@gmail.com>
2025-01-23 17:00:59 -0500

    cleaned and added some features to player
    cleaned up player.gd, added functions instead of putting it all under physics_process
    
    the sprite now has acceleration, friction, and you can also control how high the sprite jumps depending on how long you hold the space bar down.

--- newgame/player.gd
+++ newgame/player.gd
@@ -2,24 +2,37 @@ extends CharacterBody2D #extends all of the stuff from CharacterBody2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0  # negative going up for y, positive going down for y. x is normal left right
+const ACCELERATION = 600
+const FRICTION = 600
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+func _physics_process(delta: float) -> void: # delta i think is time? not too sure
+	apply_gravity(delta)
+	handle_jump()
+	
+	var input_axis := Input.get_axis("ui_left", "ui_right")
+	handle_acceleration(input_axis, delta)
+	apply_friction(input_axis, delta)
+	move_and_slide() # takes velocity and moves character by that velocity while also colliding (sliding) on static bodies
+# move_and_slide multiplies by delta
+
+func apply_gravity(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"): # ui_accept is the space key
-		# if add and is_on_floor() to the if statement, can only jump when on floor
-		velocity.y = JUMP_VELOCITY # which is -300
+func handle_jump():
+	if is_on_floor(): # different heights in jumps depending on how long space bar is held down
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if Input.is_action_just_released("ui_accept") and velocity.y < 0:
+			velocity.y = JUMP_VELOCITY/3.5
 
-	move_and_slide()
+func apply_friction(input_axis, delta):
+	if input_axis == 0:
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+
+func handle_acceleration(input_axis, delta):
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, SPEED*input_axis, ACCELERATION*delta)
