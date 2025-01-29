2b55374d586bb9d6fe93d47ba02ea83de4cc9c76
leol07 <litianle772008@gmail.com>
2025-01-29 16:27:06 -0500

    Added individual collision shapes
    individual collision shapes can now be programmed independently. They are instanced into the parallax world when needed, and placeholder code in the parallax world's script shows how to add multiple of these individual collision shapes without having to instance all of them in parallax.

--- scripts/world/parallax.gd
+++ scripts/world/parallax.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
-@onready var collision_polygon_2d: CollisionPolygon2D = $StaticBody2D/CollisionPolygon2D # access to the notes
-@onready var polygon_2d: Polygon2D = $StaticBody2D/CollisionPolygon2D/Polygon2D
+# Load the block scene
+var block_scene = preload("res://scenes/world/square_block.tscn")
 
-func _ready():
-	polygon_2d.polygon = collision_polygon_2d.polygon # set properties on nodes or get properties on those nodes
+func _ready():  # this is just placeholder code demonstrating how you can code the creation of multiple blocks, such that you don't have to instance them into parallax every time.
+	for i in range(5):  # Create 5 blocks
+		var block = block_scene.instantiate()
+		block.position = Vector2(i * 100, 200)  # Position them with some space between
+		add_child(block)  # Add the block to the scene
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
---
+++ scripts/world/static_body_2d.gd
@@ -0,0 +1,4 @@
+extends StaticBody2D
+
+func _ready():
+	draw_rect(Rect2(Vector2.ZERO, Vector2(32, 32)), Color(1, 0, 0))  # Red block
