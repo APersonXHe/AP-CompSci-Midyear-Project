9e98b38e6916462010e516f0b9344532a1aad9d6
leol07 <litianle772008@gmail.com>
2025-02-04 12:30:01 -0500

    just fixed some of the blocks in the game, made floor bigger and adjusted blocks

--- scripts/world/parallax.gd
+++ scripts/world/parallax.gd
@@ -6,7 +6,7 @@ var block_scene = preload("res://scenes/world/square_block.tscn")
 func _ready():  # this is just placeholder code demonstrating how you can code the creation of multiple blocks, such that you don't have to instance them into parallax every time.
 	for i in range(5):  # Create 5 blocks
 		var block = block_scene.instantiate()
-		block.position = Vector2(i * 100, 200)  # Position them with some space between
+		block.position = Vector2(i * 100, 160)  # Position them with some space between
 		add_child(block)  # Add the block to the scene
 
 
