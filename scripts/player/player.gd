ba078081f092192e2f6fce5d0e8a94a835d79f5a
leol07 <litianle772008@gmail.com>
2025-02-10 16:08:15 -0500

    adjust positioning of block and attempted to have player die when hitting spike but failed
    adjusted positioning of blocks, and attempted to have the player die when hitting a spike. still does not work, all the code should be in the player.gd but it is still buggy

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,8 +3,12 @@ extends CharacterBody2D #extends all of the stuff from CharacterBody2D
 const SPEED = 120.0
 const JUMP_VELOCITY = -300.0  # negative going up for y, positive going down for y. x is normal left right
 const FRICTION = 600
+var start_position : Vector2
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _ready():
+	start_position = position
+	
 func _physics_process(delta: float) -> void: # delta i think is time? not too sure
 
 	handle_jump()
@@ -14,16 +18,38 @@ func _physics_process(delta: float) -> void: # delta i think is time? not too su
 
 	apply_friction(delta)
 	move_and_slide() # takes velocity and moves character by that velocity while also colliding (sliding) on static bodies
+	if is_on_spike():
+		reset_game()
 # move_and_slide multiplies by delta
+
 
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+ 
 func handle_jump():
-	if Input.is_action_just_pressed("ui_accept"):
-		velocity.y = JUMP_VELOCITY
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
 
 
 func apply_friction(delta):
 	velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+	
+func is_on_spike() -> bool:
+	# Check for collision with spikes
+	var collision = get_slide_collision(0)
+	if collision:
+		var collider = collision.get_collider()
+		# If the player collides with a spike, return true
+		if collider and collider.name == "spike":
+			print("Collision with spike detected")
+			return true
+	return false
+
+# Reset the game and respawn the player at the start position
+func reset_game():
+	# You can reset the player position here
+	position = start_position
+	# Optionally, reset the background or other game elements
+	# get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
--- scripts/world/parallax.gd
+++ scripts/world/parallax.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
 # Load the block scene
-var block_scene = preload("res://scenes/world/square_block.tscn")
+var block_scene = preload("res://scenes/world/spike.tscn")
 
 func _ready():  # this is just placeholder code demonstrating how you can code the creation of multiple blocks, such that you don't have to instance them into parallax every time.
 	for i in range(5):  # Create 5 blocks
 		var block = block_scene.instantiate()
-		block.position = Vector2(i * 100, 160)  # Position them with some space between
+		block.position = Vector2((i+1) * 100, 160)  # Position them with some space between
 		add_child(block)  # Add the block to the scene
 
 
