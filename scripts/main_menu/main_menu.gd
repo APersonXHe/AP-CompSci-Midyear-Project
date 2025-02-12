c4ae52dbe54d02898b3b7cf87b1f434beab4ad1d
Xinyang He <APersonXHe@gmail.com>
2025-02-11 22:54:45 -0500

    main menu now chooses one of two random songs to play

--- scripts/main_menu/main_menu.gd
+++ scripts/main_menu/main_menu.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
+var songs = [
+	"res://assets/audio/F-777 - Dance of The Violins.mp3",
+	"res://assets/audio/EVADE FROM 宇宙 - Flyday Chinatown Remix_Instrumental.mp3" 
+]
+
 func _ready() -> void:
+	var song_to_play = songs[randi() % songs.size()]  # Randomly choose one of the songs
 	GameManager.current_level = "res://scenes/main_menu/main_menu.tscn"
 	GameManager.player_dead = false
-	AudioManager.play_music("res://assets/audio/F-777 - Dance of The Violins.mp3")
+	GameManager.player_won = false
+	AudioManager.play_music(song_to_play)
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu/level_select.tscn")
